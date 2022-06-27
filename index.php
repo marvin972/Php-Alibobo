4490059c1cc5806c62cd5dad85832aaf3957af8e
Marvin Virapin <marvin.virapin@hotmail.fr>
2022-06-27 15:11:48 +0200

    Ajout des includes

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+/* Utiliser include ou require
+include renvoie un avertisement simple en cas d'erreur
+require renvoie une erreur fatale et arrête l'éxécution du scripte */
+
+include './includes/header.php';
+include './includes/main.php';
+include './includes/footer.php';
+
