0c4f13d80f415b5e431c4067895cc17cc6d23d27
Marvin Virapin <marvin.virapin@hotmail.fr>
2022-06-27 15:22:47 +0200

    Ajout structure html

--- includes/footer.php
+++ includes/footer.php
@@ -1,1 +1,17 @@
-<h1>Footer</h1>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <footer>
+        <h1>Footer</h1>
+    </footer>
+</body>
+
+</html>
