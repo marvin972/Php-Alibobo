22f7a1d7f6ff3dd2b189b50d3390dc09940d9d97
Marvin Virapin <marvin.virapin@hotmail.fr>
2022-07-04 14:10:14 +0200

    Ajout page dans includes et fonction autoinclude

--- includes/main.php
+++ includes/main.php
@@ -2,8 +2,21 @@
         <h1>Main</h1>
         <?php
 
-dump($_GET);
+// // Opérateur ternaire
+// $page = isset($_GET['page']) ? $_GET['page'] : "accueil";
 
+// // Opérateur conventionnelle
+// if (isset($_GET['page'])){
+//     $page = $_GET['page'];
+//     dump($page);
+// }
+// else {
+//     $page = "accueil";
 
+// }
+
+// Null coalescent operator
+$page = $_GET['page'] ?? "accueil";
+autoInclude($page);
         ?>
     </main>
